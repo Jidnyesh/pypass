e556ac757371c3ea10729df079d267b1cf2c5acf
Jidnyesh <32982506+Jidnyesh@users.noreply.github.com>
2019-02-19 18:36:30 +0530

    Add files via upload
    Main program file(.py)

---
+++ pypass.py
@@ -0,0 +1,45 @@
+"""
+This is a module to generate random password of different length for your project
+"""
+
+import random
+
+#String module used to get all the upper and lower alphabet in ascii
+import string
+
+#Declaring strings used in password
+special_symbols = "!@#$%^&*()_+{}|:<>?-=[]\;',./"
+alphabets = string.ascii_letters
+numbers = string.digits
+
+
+
+def randompasswordgenerator():
+
+    #Taking inputs for information of password
+    n = int(input("Enter the length of password:"))
+    print("1:Alpha \n2:Alpha numeric \n3:Aplha symbol numeric")
+    choice = int(input("Choose the type of password:\n"))
+
+    #Making a empty list to store passwords
+    passw = []
+
+    #Setting value of str_list to combination according to choice
+    if choice == 1:
+        str_list = alphabets
+    elif choice == 2:
+        str_list = alphabets + numbers
+    elif choice == 3:
+        str_list = special_symbols + alphabets + numbers
+
+    for x in range(n):
+        rnd = random.choice(str_list)
+        passw.append(rnd)
+
+    password = "".join(passw)
+
+    print(password)
+
+#Function call
+if __name__=="__main__":
+    randompasswordgenerator()
